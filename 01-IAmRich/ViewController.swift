fc842edc366a3174fcafdf05e1f2aaf4e8f8cdc3
17-wilson <whdeveloper17@gmail.com>
2019-02-28 15:21:03 -0500

    Initial Commit

---
+++ 01-IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-IAmRich
+//
+//  Created by Geomatic Technologies on 2/28/19.
+//  Copyright © 2019 Geomatic Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
