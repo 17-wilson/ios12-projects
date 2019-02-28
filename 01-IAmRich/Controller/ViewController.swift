4d70d34e9bfb97a6d13119e04792bd1920a5bfe0
17-wilson <whdeveloper17@gmail.com>
2019-02-28 15:56:38 -0500

    * hemos creado un print  dentro el view controlar * hemos creado y dos carpetas y movidos los archivos

--- 01-IAmRich/Controller/ViewController.swift
+++ 01-IAmRich/Controller/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hola mundo")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
